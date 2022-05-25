9efb9962b4ffe0df13e2b4a2976c7448db0e22f6
yuvron <yuvr99@gmail.com>
2022-05-25 17:41:22 +0300

    Initialize Project

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const ts = require("gulp-typescript");
+const tsProject = ts.createProject("tsconfig.json");
+const browsersync = require("browser-sync").create();
+
+// HTML task
+gulp.task("html", () => {
+	return gulp.src("src/*.html").pipe(gulp.dest("dist"));
+});
+
+// Typescript task
+gulp.task("ts", () => {
+	return gulp.src("src/*.ts").pipe(tsProject()).pipe(gulp.dest("dist"));
+});
+
+// Sass task
+gulp.task("sass", () => {
+	return gulp.src("src/style.scss").pipe(sass()).pipe(gulp.dest("dist"));
+});
+
+// Watch task
+gulp.task("watch", () => {
+	gulp.watch(["src/*.html", "src/*.ts"], gulp.series("html", "ts", "browsersyncReload"));
+	gulp.watch("src/style.scss", gulp.series("sass", "browsersyncReload"));
+});
+
+// Browser sync tasks
+gulp.task("browsersyncServe", (cb) => {
+	browsersync.init({
+		port: 5500,
+		server: { baseDir: "./dist" },
+	});
+	cb();
+});
+
+gulp.task("browsersyncReload", (cb) => {
+	browsersync.reload();
+	cb();
+});
+
+// Default task
+gulp.task("default", gulp.series("html", "ts", "sass", "browsersyncServe", "watch"));
