1fe9b1aabcb3366265bdf132ffeac1bec118ac51
yuvron <yuvr99@gmail.com>
2022-05-26 13:57:07 +0300

    Basic Tasks logic

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+const newTaskContent = document.getElementById("new-content") as HTMLInputElement;
+const newTaskButton = document.getElementById("new-button");
+
+newTaskButton.addEventListener("click", () => {
+	const tasks = document.getElementsByClassName("task");
+	const lastTask = [...tasks].pop();
+	lastTask.insertAdjacentElement("afterend", createTask(newTaskContent.value));
+	newTaskContent.value = "";
+});
+
+// Create new Task
+function createTask(text: string): HTMLElement {
+	const newTask = document.createElement("div");
+	newTask.classList.add("task");
+	// Create checkbox
+	const checkbox = document.createElement("input");
+	checkbox.type = "checkbox";
+	checkbox.classList.add("checkbox");
+	newTask.appendChild(checkbox);
+	// Create text
+	const taskContent = document.createElement("span");
+	taskContent.innerText = text;
+	newTask.appendChild(taskContent);
+	// Create trash button
+	const trash = document.createElement("button");
+	trash.innerHTML = "<i class='fa-solid fa-trash'></i>";
+	trash.addEventListener("click", () => deleteTask(newTask));
+	newTask.appendChild(trash);
+	return newTask;
+}
+
+function deleteTask(task: HTMLElement) {
+	task.remove();
+}
