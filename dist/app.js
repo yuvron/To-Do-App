ba6d66953c05f8d1c259a3f161e96d1f7c46dbc4
yuvron <yuvr99@gmail.com>
2022-05-26 17:03:59 +0300

    Delete, edit, arrows,new task

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,17 @@
+var icons;
+(function (icons) {
+    icons["edit"] = "<i class='fa-solid fa-pen'></i>";
+    icons["delete"] = "<i class='fa-solid fa-trash'></i>";
+})(icons || (icons = {}));
 const newTaskContent = document.getElementById("new-content");
 const newTaskButton = document.getElementById("new-button");
 newTaskButton.addEventListener("click", () => {
-    const tasks = document.getElementsByClassName("task");
-    const lastTask = [...tasks].pop();
-    lastTask.insertAdjacentElement("afterend", createTask(newTaskContent.value));
-    newTaskContent.value = "";
+    if (newTaskContent.value.length > 0) {
+        const tasks = document.getElementsByClassName("task");
+        const lastTask = [...tasks].pop();
+        lastTask.insertAdjacentElement("afterend", createTask(newTaskContent.value));
+        newTaskContent.value = "";
+    }
 });
 // Create new Task
 function createTask(text) {
@@ -12,20 +19,31 @@ function createTask(text) {
     newTask.classList.add("task");
     // Create checkbox
     const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
+    checkbox.type = "radio";
     checkbox.classList.add("checkbox");
     newTask.appendChild(checkbox);
     // Create text
     const taskContent = document.createElement("span");
+    text = formatText(text);
     taskContent.innerText = text;
     newTask.appendChild(taskContent);
+    // Create edit button
+    const edit = document.createElement("button");
+    edit.classList.add("edit");
+    edit.innerHTML = icons.edit;
+    edit.addEventListener("click", () => deleteTask(newTask));
+    newTask.appendChild(edit);
     // Create trash button
     const trash = document.createElement("button");
-    trash.innerHTML = "<i class='fa-solid fa-trash'></i>";
+    trash.classList.add("delete");
+    trash.innerHTML = icons.delete;
     trash.addEventListener("click", () => deleteTask(newTask));
     newTask.appendChild(trash);
     return newTask;
 }
 function deleteTask(task) {
     task.remove();
+}
+function formatText(text) {
+    return text.charAt(0).toUpperCase() + text.substring(1);
 }
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
+enum icons {
+	edit = "<i class='fa-solid fa-pen'></i>",
+	delete = "<i class='fa-solid fa-trash'></i>",
+}
+
 const newTaskContent = document.getElementById("new-content") as HTMLInputElement;
 const newTaskButton = document.getElementById("new-button");
 
 newTaskButton.addEventListener("click", () => {
-	const tasks = document.getElementsByClassName("task");
-	const lastTask = [...tasks].pop();
-	lastTask.insertAdjacentElement("afterend", createTask(newTaskContent.value));
-	newTaskContent.value = "";
+	if (newTaskContent.value.length > 0) {
+		const tasks = document.getElementsByClassName("task");
+		const lastTask = [...tasks].pop();
+		lastTask.insertAdjacentElement("afterend", createTask(newTaskContent.value));
+		newTaskContent.value = "";
+	}
 });
 
 // Create new Task
@@ -14,16 +21,24 @@ function createTask(text: string): HTMLElement {
 	newTask.classList.add("task");
 	// Create checkbox
 	const checkbox = document.createElement("input");
-	checkbox.type = "checkbox";
+	checkbox.type = "radio";
 	checkbox.classList.add("checkbox");
 	newTask.appendChild(checkbox);
 	// Create text
 	const taskContent = document.createElement("span");
+	text = formatText(text);
 	taskContent.innerText = text;
 	newTask.appendChild(taskContent);
+	// Create edit button
+	const edit = document.createElement("button");
+	edit.classList.add("edit");
+	edit.innerHTML = icons.edit;
+	edit.addEventListener("click", () => deleteTask(newTask));
+	newTask.appendChild(edit);
 	// Create trash button
 	const trash = document.createElement("button");
-	trash.innerHTML = "<i class='fa-solid fa-trash'></i>";
+	trash.classList.add("delete");
+	trash.innerHTML = icons.delete;
 	trash.addEventListener("click", () => deleteTask(newTask));
 	newTask.appendChild(trash);
 	return newTask;
@@ -31,4 +46,8 @@ function createTask(text: string): HTMLElement {
 
 function deleteTask(task: HTMLElement) {
 	task.remove();
+}
+
+function formatText(text: string): string {
+	return text.charAt(0).toUpperCase() + text.substring(1);
 }
